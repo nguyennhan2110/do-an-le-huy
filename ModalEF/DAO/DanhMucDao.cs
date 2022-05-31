c10c5446cc10ae8f28856ad079c0a6f544938928
nguyennhan2110 <57670976+nguyennhan2110@users.noreply.github.com>
2022-05-31 23:58:59 +0700

    final-danhmuc

---
+++ ModalEF/DAO/DanhMucDao.cs
@@ -0,0 +1,46 @@
+﻿using ModalEF.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalEF.DAO
+{
+    public class DanhMucDao
+    {
+        ContextDB db = null;
+
+        public DanhMucDao()
+        {
+            db = new ContextDB();
+        }
+
+        //Phân trang
+        public IEnumerable<DanhMuc> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<DanhMuc> model = db.DanhMucs;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.TenDanhMuc.Contains(searchString));
+            }
+
+            return model.OrderByDescending(x => x.NgayTao).ToPagedList(page, pageSize);
+        }
+
+        //Xoá
+        public bool Delete(int id)
+        {
+            try
+            {
+                var dm = db.DanhMucs.Find(id);
+                db.DanhMucs.Remove(dm);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            { return false; }
+        }
+    }
+}
---
+++ timdothatlac/Areas/Admin/Controllers/DanhMucController.cs
@@ -0,0 +1,108 @@
+﻿using ModalEF.DAO;
+using ModalEF.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using timdothatlac.Common;
+
+namespace timdothatlac.Areas.Admin.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        private ContextDB db = new ContextDB();
+
+        //Phân trang list, default = 5
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new DanhMucDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+
+            ViewBag.SearchString = searchString;
+
+            return View(model);
+        }
+
+        //Thêm
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDanhMuc,TenDanhMuc,NgayTao")] DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                danhMuc.NgayTao = DateTime.Now;
+                db.DanhMucs.Add(danhMuc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(danhMuc);
+        }
+
+        //Sửa
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMucs.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDanhMuc,TenDanhMuc,NgayTao")] DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(danhMuc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(danhMuc);
+        }
+
+        //Xoá
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new DanhMucDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        //Xem chi tiết
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMucs.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        //Đăng xuất
+        public ActionResult Logout()
+        {
+            Session[Constant.USER_SESSION] = null;
+            return RedirectToAction("Index", "Login", routeValues: new { Area = "" });
+        }
+    }
+}
