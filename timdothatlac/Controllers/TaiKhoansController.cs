267d6cdd0b1937f8d4ef13c26b69b5574ab0bb4b
nguyennhan2110 <57670976+nguyennhan2110@users.noreply.github.com>
2022-06-22 01:48:17 +0700

    fix-client

--- timdothatlac/Controllers/TaiKhoansController.cs
+++ timdothatlac/Controllers/TaiKhoansController.cs
@@ -1,6 +1,8 @@
 ﻿using ModalEF.EF;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -25,6 +27,49 @@ namespace timdothatlac.Controllers
             {
                 return HttpNotFound();
             }
+            return View(taiKhoan);
+        }
+
+        //Sửa
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaQuyen = new SelectList(db.Quyens, "MaQuyen", "TenQuyen", taiKhoan.MaQuyen);
+            return View(taiKhoan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TaiKhoan taiKhoan, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Server.MapPath("~/FileUpload");
+                try
+                {
+                    if (file != null)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string pathFull = Path.Combine(path, fileName);
+                        file.SaveAs(pathFull);
+                        taiKhoan.AnhDaiDien = file.FileName;
+                    }
+
+                    db.Entry(taiKhoan).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index", new { id = taiKhoan.MaTaiKhoan });
+                }
+                catch (Exception e) { }
+            }
+            ViewBag.MaQuyen = new SelectList(db.Quyens, "MaQuyen", "TenQuyen", taiKhoan.MaQuyen);
             return View(taiKhoan);
         }
 
